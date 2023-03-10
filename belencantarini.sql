916fc58c24fe352127088d741409404800f875c0
Belen Cantarini <belencantarini@gmail.com>
2023-03-10 08:13:58 -0300

    php intermedio

--- belencantarini.sql
+++ belencantarini.sql
@@ -1,40 +1,111 @@
--- Crear base
-create database belencantarini;
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: belencantarini
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.27-MariaDB
 
--- Utilizar base
-use belencantarini;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Crear tabla
-create table consultas
-(
-id INT(11) not null auto_increment primary key,
-nombre VARCHAR(50) not null,
-email VARCHAR(50) not null,
-mensaje text not null
-)
+--
+-- Table structure for table `administradores`
+--
 
--- Crear tabla
-create table tratamientos
-(
-id INT(11) not null auto_increment primary key,
-tratamiento VARCHAR(50) not null,
-tarifa INT not null,
-imagen text,
-descripcion text
-)
+DROP TABLE IF EXISTS `administradores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `administradores` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `usuario` varchar(20) NOT NULL,
+  `clave` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `usuario` (`usuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- borrar tabla imagen
-drop table tratamientos;
+--
+-- Dumping data for table `administradores`
+--
 
--- Modificar 
--- update tratamientos set imagen='https://media.istockphoto.com/id/606238302/es/foto/linda-ni%C3%B1a-recibiendo-tratamiento-con-%C3%A1cido-hialur%C3%B3nico.jpg?s=612x612&w=0&k=20&c=e0-eOiDgqmYCmZPHJ0m0c5dOtPlbPTk-jP3EmwAXV_o=' where id='1';
+LOCK TABLES `administradores` WRITE;
+/*!40000 ALTER TABLE `administradores` DISABLE KEYS */;
+INSERT INTO `administradores` VALUES (1,'admin','admin123');
+/*!40000 ALTER TABLE `administradores` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `consultas`
+--
 
-create table administradores
-(
-id INT(11) not null auto_increment primary key,
-usuario VARCHAR(20) not null UNIQUE,
-clave VARCHAR(255) not null
-)
+DROP TABLE IF EXISTS `consultas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `consultas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `mensaje` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-insert into administradores (usuario, clave) values ('admin', 'admin123');
+--
+-- Dumping data for table `consultas`
+--
+
+LOCK TABLES `consultas` WRITE;
+/*!40000 ALTER TABLE `consultas` DISABLE KEYS */;
+INSERT INTO `consultas` VALUES (1,'Belen','belencantarini@gmail.com','Hola! Quiero saber mas!'),(4,'Damian','damian.gdl@hotmail.com','Hola Dami'),(5,'belen','cantarini@gmail.com','hola'),(6,'belen','cantarini@gmail.com','hola'),(7,'Belen','belencantarini@gmail.com','Probando include conexion');
+/*!40000 ALTER TABLE `consultas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tratamientos`
+--
+
+DROP TABLE IF EXISTS `tratamientos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tratamientos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tratamiento` varchar(50) NOT NULL,
+  `tarifa` int(11) NOT NULL,
+  `imagen` text DEFAULT NULL,
+  `descripcion` text DEFAULT NULL,
+  `estado` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=85 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tratamientos`
+--
+
+LOCK TABLES `tratamientos` WRITE;
+/*!40000 ALTER TABLE `tratamientos` DISABLE KEYS */;
+INSERT INTO `tratamientos` VALUES (9,'Hilos tensores',15000,'https://http2.mlstatic.com/D_NQ_NP_777336-MLA52754808975_122022-O.webp','Resistente al agua, con fuerte elasticidad, tu cara no estará apretada. Levanta y reafirma inmediatamente la piel para crear una cara en forma de V.\r\nLevantamiento: el sistema de elevación de cara y cuello mejora la piel suelta y elimina las arrugas en la cara, la barbilla del cuello y el área de los ojos.\r\nDebe encoger los músculos faciales flacidos y dar forma a su cara. Ayuda a dar forma a una cara delgada y firme y a crear una cara hermosa.\r\nEs ideal para ocasiones especiales, bodas, reuniones, modelado, fotografía, viajes, etc.','finalizado'),(81,'Botox',15500,'./imagenes/replication (3)0.png','Atenua o elimina arrugas de expresion','finalizado'),(84,'Botox',12000,'./imagenes/replication (2)0.png','Empeza hoy ese tratamiento que siempre quisiste!!!','finalizado');
+/*!40000 ALTER TABLE `tratamientos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'belencantarini'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-03-10  8:11:52
